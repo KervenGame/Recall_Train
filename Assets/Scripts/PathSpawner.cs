79ef41415e47a76b04e29f2d47adab111e4cea7f
hankangwen <1397796710@qq.com>
2023-05-25 20:31:49 +0800

    Update PathSpawner.cs

--- Assets/Scripts/PathSpawner.cs
+++ Assets/Scripts/PathSpawner.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,33 +5,33 @@ public class PathSpawner : MonoBehaviour
 {
     public List<Vector3> pathPositions;
     public List<Vector3> pathRotations;
-    Rigidbody rb;
-    private float recordFrequency = 0.05f;
-    private float recordTimer;
-    private float recallMaxTime = 10.0f;
-    private float recallTimer;
+    Rigidbody _rb;
+    private readonly float _recordFrequency = 0.05f;
+    private float _recordTimer;
+    private readonly float _recallMaxTime = 10.0f;
+    private float _recallTimer;
     [SerializeField] private Vector3 destPosition;
     [SerializeField] private Vector3 destRotation;
-    private int maxPathPoints = 120;
-    bool isMoving = false;
-    bool isRecalling = false;
+    private readonly int _maxPathPoints = 120;
+    private bool _isMoving;
+    private bool _isRecalling;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
 
-        resetRecordTimer();
-        resetRecallTimer();
+        ResetRecordTimer();
+        ResetRecallTimer();
     }
 
     // private void Update()
     private void FixedUpdate()
     {
-        // If the object is in recalling, then other regualar movements are disabled
-        if (isRecalling)
+        // If the object is in recalling, then other regular movements are disabled
+        if (_isRecalling)
         {
             // disable the sphere's self move physic
-            rb.isKinematic = true;
+            _rb.isKinematic = true;
 
             float distance = Vector3.Distance(transform.position, destPosition);
             // If arrived at the last path point, update the destination
@@ -43,12 +42,13 @@ public class PathSpawner : MonoBehaviour
             else
             {
                 // recall position
-                float speed = distance / recordFrequency;
-                transform.position = Vector3.MoveTowards(transform.position, destPosition, speed * Time.deltaTime);
+                float speed = distance / _recordFrequency;
+                Transform transform1;
+                (transform1 = transform).position = Vector3.MoveTowards(transform.position, destPosition, speed * Time.deltaTime);
 
                 // recall rotation
-                float angle = Quaternion.Angle(transform.rotation, Quaternion.Euler(destRotation));
-                float angleSpeed = angle / recordFrequency;
+                float angle = Quaternion.Angle(transform1.rotation, Quaternion.Euler(destRotation));
+                float angleSpeed = angle / _recordFrequency;
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(destRotation), angleSpeed * Time.deltaTime);
             }
         }
@@ -56,41 +56,42 @@ public class PathSpawner : MonoBehaviour
         // If the object is not in recalling, record the path points if it is moving
         else
         {
-            rb.isKinematic = false;
-            isMoving = rb.velocity.magnitude == 0 ? false : true;
+            _rb.isKinematic = false;
+            _isMoving = _rb.velocity.magnitude != 0;
 
-            if (isMoving)
+            if (_isMoving)
             {
                 // For every recordFrequency time, record a path point with position and rotation
-                recordTimer -= Time.deltaTime;
+                _recordTimer -= Time.deltaTime;
 
-                if (recordTimer <= 0)
+                if (_recordTimer <= 0)
                 {
-                    RecordPathPoint(transform.position, transform.eulerAngles);
-                    resetRecordTimer();
+                    var transform1 = transform;
+                    RecordPathPoint(transform1.position, transform1.eulerAngles);
+                    ResetRecordTimer();
                 }
             }
 
-            // if the object stays for a long time, start remove the path points from the begining
+            // if the object stays for a long time, start remove the path points from the beginning
             // every recordFrequency time
             else
             {
-                recallTimer -= Time.deltaTime;
-                if (recallTimer <= 0)
+                _recallTimer -= Time.deltaTime;
+                if (_recallTimer <= 0)
                 {
                     if (pathPositions.Count > 0 && pathRotations.Count > 0)
                     {
-                        recordTimer -= Time.deltaTime;
-                        if (recordTimer <= 0)
+                        _recordTimer -= Time.deltaTime;
+                        if (_recordTimer <= 0)
                         {
                             pathPositions.RemoveAt(0);
                             pathRotations.RemoveAt(0);
-                            resetRecordTimer();
+                            ResetRecordTimer();
                         }
                     }
                     else
                     {
-                        resetRecallTimer();
+                        ResetRecallTimer();
                     }
                 }
 
@@ -106,8 +107,8 @@ public class PathSpawner : MonoBehaviour
      */
     private void RecordPathPoint(Vector3 position, Vector3 rotation)
     {
-        // if there are too many pathpoints, remove the oldest one
-        if (pathPositions.Count >= maxPathPoints && pathRotations.Count >= maxPathPoints)
+        // if there are too many path points, remove the oldest one
+        if (pathPositions.Count >= _maxPathPoints && pathRotations.Count >= _maxPathPoints)
         {
             pathPositions.RemoveAt(0);
             pathRotations.RemoveAt(0);
@@ -127,33 +128,33 @@ public class PathSpawner : MonoBehaviour
     {
         if (pathPositions.Count <= 0 || pathRotations.Count <= 0)
         {
-            isRecalling = false;
+            _isRecalling = false;
             return;
         }
 
-        destPosition = pathPositions[pathPositions.Count - 1];
-        destRotation = pathRotations[pathRotations.Count - 1];
+        destPosition = pathPositions[^1];
+        destRotation = pathRotations[^1];
 
         pathPositions.RemoveAt(pathPositions.Count - 1);
         pathRotations.RemoveAt(pathRotations.Count - 1);
-        isRecalling = true;
+        _isRecalling = true;
     }
 
     /**
      *
      * Reset the record timer
      */
-    private void resetRecordTimer()
+    private void ResetRecordTimer()
     {
-        recordTimer = recordFrequency;
+        _recordTimer = _recordFrequency;
     }
 
     /**
      *
      * Reset the recall timer
      */
-    private void resetRecallTimer()
+    private void ResetRecallTimer()
     {
-        recallTimer = recallMaxTime;
+        _recallTimer = _recallMaxTime;
     }
 }
